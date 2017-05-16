c6b4ab64f46dc341eeeb5559c11c350c728e257f
DESKTOP-C1PQH24\patin <patino37@yahoo.com>
2017-05-16 18:43:42 -0400

    Modified the home page and added text fields and button to AddNewMember page. Also, added validation to phone number field on AddNewMember page.

--- app/src/main/java/com/example/yahia/rewards_program/AddNewMember.java
+++ app/src/main/java/com/example/yahia/rewards_program/AddNewMember.java
@@ -2,12 +2,29 @@ package com.example.yahia.rewards_program;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddNewMember extends AppCompatActivity {
+
+    EditText newPhoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_member);
+
+        newPhoneNumber = (EditText)findViewById(R.id.newPhoneNumber);
+
+        newPhoneNumber.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus){
+                if(newPhoneNumber.getText().length() < 10)
+                {
+                    newPhoneNumber.setError("Invalid Phone #");
+                }
+            }
+        });
+
     }
 }
