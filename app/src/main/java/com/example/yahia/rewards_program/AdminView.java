db04796e5e6d6a211474b906f15ba28fe5e2e732
Yahia <Seidiy15@students.ecu.edu>
2017-05-15 23:17:03 -0400

    Made the admin Java, layout, changed the nortification button to read "admin view", also added the title.

---
+++ app/src/main/java/com/example/yahia/rewards_program/AdminView.java
@@ -0,0 +1,18 @@
+package com.example.yahia.rewards_program;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yahia on 5/15/2017.
+ */
+
+public class AdminView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_view);
+    }
+}
+
+
