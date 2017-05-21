3bce1c551a91f4598fd6ece85cecd18e2c0037e9
lquick94 <quickl14@students.ecu.edu>
2017-05-21 16:06:30 -0500

    color change

---
+++ app/src/main/java/com/example/yahia/rewards_program/Settings.java
@@ -0,0 +1,13 @@
+package com.example.yahia.rewards_program;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
