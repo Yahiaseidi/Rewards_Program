c825bbb08829eec1095bead27de3ce8fb8db4583
Leslie O'Donnell <quickl14@students.ecu.edu>
2017-06-08 07:12:17 -0400

    Create AddNewMember.java
    I know it's bad practice but I've tried everything else so I'm just copying and pasting my code now.

--- app/src/main/java/com/example/yahia/rewards_program/AddNewMember.java
+++ app/src/main/java/com/example/yahia/rewards_program/AddNewMember.java
@@ -1,5 +1,7 @@
 package com.example.yahia.rewards_program;
 
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,25 +14,21 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.content.Intent;
-import android.support.design.internal.BottomNavigationItemView;
-import android.support.design.internal.BottomNavigationMenuView;
-import android.support.design.widget.BottomNavigationView;
 import android.util.Log;
+import java.net.MalformedURLException;
+import java.util.concurrent.ExecutionException;
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
 
-import com.amazonaws.auth.CognitoCachingCredentialsProvider;
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+public class AddNewMember extends AppCompatActivity implements View.OnClickListener{
 
-import java.lang.reflect.Field;
+    private EditText newPhoneNumber;
+    private EditText cardNumber;
+    private Button createNewMember_btn;
+    private MobileServiceClient mClient;
+    private MobileServiceTable<Users> mUsersTable;
 
-public class AddNewMember extends AppCompatActivity implements View.OnClickListener {
-
-    EditText newPhoneNumber;
-    EditText cardNumber;
-    Button createNewMember_btn;
 
 
     @Override
@@ -130,43 +128,86 @@ public class AddNewMember extends AppCompatActivity implements View.OnClickListe
                 return false;
             }
         });
+
+        try {
+            mClient = new MobileServiceClient(
+                    "https://rewards-program.azurewebsites.net",
+                    this);
+
+
+            mUsersTable = mClient.getTable(Users.class);
+            newPhoneNumber = (EditText) findViewById(R.id.newPhoneNumber);
+            cardNumber = (EditText) findViewById(R.id.enterID_txt);
+
+
+        } catch (MalformedURLException e) {
+            //  createAndShowDialog(new Exception("There was an error creating the Mobile Service. Verify the URL"), "Error");
+        } catch (Exception e){
+            //createAndShowDialog(e, "Error");
+        }
+
     }
+
 
     @Override
     public void onClick(View v) {
         if (v == createNewMember_btn)
         {
-            final String phone = newPhoneNumber.getText().toString();
-            final String card_id = cardNumber.getText().toString();
-            Runnable runnable = new Runnable() {
-                public void run() {
 
-                    CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
-                            getApplicationContext(),
-                            "us-east-1:58f4f084-a42c-48ee-beb1-b78e5058e168", // Identity Pool ID
-                            Regions.US_EAST_1 // Region
-                    );
-
-                    AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(credentialsProvider);
-                    DynamoDBMapper mapper = new DynamoDBMapper(ddbClient);
-                    //Here you make a new user object and either use my getters or setters to get info and change info as you want.
-                    users user = new users();
-                    user.setCard_id(card_id);
-                    user.setPhone_number(phone);
-                    user.setReward_points("0");
-                    mapper.save(user);
-                }
-            };
-            new Thread(runnable).start();
+            addItem();
             goToMemberAdded();
 
-        }
 
+        }
     }
 
     public void goToMemberAdded() {
         Intent newActivity = new Intent(this, MemberAdded.class);
         startActivity(newActivity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    public void addItem() {
+        if (mClient == null) {
+            return;
+        }
+
+        final Users item = new Users();
+        item.setNumber(newPhoneNumber.getText().toString());
+        item.setCard(cardNumber.getText().toString());
+        item.setPoints("0");
+
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    final Users entity = addItemInTable(item);
+                } catch (final Exception e) {
+                    //createAndShowDialogFromTask(e, "Error");
+                }
+                return null;
+            }
+        };
+
+        runAsyncTask(task);
+    }
+
+    public Users addItemInTable(Users item) throws ExecutionException, InterruptedException {
+        Users entity = mUsersTable.insert(item).get();
+        return entity;
+    }
+
+    private AsyncTask<Void, Void, Void> runAsyncTask(AsyncTask<Void, Void, Void> task) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            return task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        } else {
+            return task.execute();
+        }
     }
 
 }
