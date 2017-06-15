9a2083d0f0e4ad16701dcb365c3d5f5259a524dd
DESKTOP-C1PQH24\patin <patino37@yahoo.com>
2017-06-14 22:05:15 -0400

    Added a progress bar to show point balance visually. Added a button that will take cashier into order screen. Added text to show the user how many points they have and how many points they need to get a reward.

--- app/src/main/java/com/example/yahia/rewards_program/MemberAccount.java
+++ app/src/main/java/com/example/yahia/rewards_program/MemberAccount.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
@@ -22,14 +24,31 @@ public class MemberAccount extends AppCompatActivity {
 
     private MobileServiceClient mClient;
     private MobileServiceTable<Users> mUsersTable;
+    private ProgressBar progressBar_points = null;
+    TextView point_total;
+    TextView points_needed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_account);
+
+        progressBar_points = (ProgressBar)findViewById(R.id.progressBar_points);
         String s = getIntent().getStringExtra("EXTRA_SESSION_ID");
-        TextView textView = (TextView) findViewById(R.id.textViewUser);
-        textView.setText("Rewards Points: " + s);
+
+        int maxPoints = 100;
+        int points = Integer.parseInt(s);
+        int pointsNeeded = maxPoints - points;
+
+        point_total = (TextView)findViewById(R.id.point_total);
+        points_needed = (TextView)findViewById(R.id.points_needed);
+
+        points_needed.setText("Points until next reward: " + pointsNeeded);
+        point_total.setText(points + " points");
+
+        progressBar_points.setVisibility(View.VISIBLE);
+        progressBar_points.setMax(100);
+        progressBar_points.setProgress(points);
 
     }
 
