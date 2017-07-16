b74b7e692f3402c3cf97cf3ada54b15ad8a76278
Yahia <Seidiy15@students.ecu.edu>
2017-07-16 19:46:39 -0400

    started doing admin.java but didnt  finish

--- app/src/main/java/com/example/yahia/rewards_program/AdminView.java
+++ app/src/main/java/com/example/yahia/rewards_program/AdminView.java
@@ -7,6 +7,9 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Yahia on 5/15/2017.
@@ -14,15 +17,21 @@ import android.view.MenuItem;
 
 public class AdminView extends AppCompatActivity {
 
+    private EditText easy;
+    private EditText medium;
+    private EditText hard;
+    private EditText target;
+    Button update_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_view);
 
-
-
-
-
+        update_btn = (Button)findViewById(R.id.update_btn);
+        easy = (EditText)findViewById(R.id.easy);
+        medium = (EditText)findViewById(R.id.medium);
+        hard = (EditText)findViewById(R.id.hard);
+        target = (EditText)findViewById(R.id.target);
 
         //Calls the helper function to stop basic android animation.
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
@@ -57,7 +66,11 @@ public class AdminView extends AppCompatActivity {
             }
         });
     }
+    public void onClick(View view) {
+        if (view == update_btn) {
 
+        }
+    }
 }
 
 
